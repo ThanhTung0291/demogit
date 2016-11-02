cdb39749f11f49c4a57f8f551c27380cedefe5f7
Tung Nguyen <tungnguyen0015@gmail.com>
2016-11-02 15:42:40 +0700

    init project

---
+++ tung/src/pingponggame.java
@@ -0,0 +1,24 @@
+import javax.swing.JFrame;
+
+/**
+ * 
+ */
+
+/**
+ * @author US
+ *
+ */
+public class pingponggame extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		pingponggame f = new pingponggame();
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		f.setVisible(true);
+
+	}
+
+}
